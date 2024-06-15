4c3107989c00a779668a92c49057b3729a3b0e2e
Gustavo Carrillo <gacch2505@icloud.com>
2024-06-15 12:11:10 -0600

    Update and delete working

--- src/components/DashboardPage.vue
+++ src/components/DashboardPage.vue
@@ -1,48 +1,112 @@
 <template>
-   <div>
-        <h2>Users Table</h2>
-        <table class="table table-dark table-hover">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Nombre</th>
-                    <th>Email</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v.for="user in users" :key="user.id">
-                    <td>{{ user.id }}</td>
-                    <td>{{ user.name }}</td>
-                    <td>{{ user.email }}</td>
-                    <td> </td>
-                </tr>
-            </tbody>
-        </table>
-   </div>
-</template>
-
-<script>
-    import axiosInstance from '@/config/axiosConfig';
-
-    export default {
-        data() {
-            return {
-                users: []
-            };
-        },
-        created() {
-            this.fetchUsers();
-        },
-        methods: {
-            async fetchUsers() {
-                try {
-                    const response = await axiosInstance.get('/showusers');
-                    this.users = response.data.users;
-                } catch (error) {
-                    console.error('Error while triying get users', error);
-                }
-            }
+    <div class="container">
+      <h2>Users Table</h2>
+      <table class="table table-hover">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Nombre</th>
+            <th>Email</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="user in users" :key="user.id">
+            <td>{{ user.id }}</td>
+            <td>{{ user.name }}</td>
+            <td>{{ user.email }}</td>
+            <td>
+              <button @click="selectUser(user)" type="button" class="btn btn-primary btn-sm">Edit</button>
+              <button @click="confirmDelete(user.id)" type="button" class="btn btn-danger btn-sm">Delete</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+  
+      <div v-if="selectedUser">
+        <h3>Edit user</h3>
+        <form @submit.prevent="saveChanges">
+          <div class="form-group">
+            <label for="userName">Name</label>
+            <input type="text" id="userName" v-model="selectedUser.name" class="form-control">
+          </div>
+          <div class="form-group">
+            <label for="userEmail">Email</label>
+            <input type="text" id="userEmail" v-model="selectedUser.email" class="form-control">
+          </div>
+          <button type="submit" class="btn btn-primary">Save Changes</button>
+          <button type="button" class="btn btn-secondary" @click="cancelEdit">Cancel</button>
+        </form>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import axiosInstance from '@/config/axiosConfig';
+  import Swal from 'sweetalert2';
+  
+  export default {
+    data() {
+      return {
+        users: [],
+        selectedUser: null
+      };
+    },
+    created() {
+      this.fetchUsers();
+    },
+    methods: {
+      async fetchUsers() {
+        try {
+          const response = await axiosInstance.get('/showusers');
+          this.users = response.data.users;
+        } catch (error) {
+          console.error('Error while trying to get users', error);
         }
-    };
-</script>
+      },
+      selectUser(user) {
+        this.selectedUser = { ...user };
+      },
+      cancelEdit() {
+        this.selectedUser = null;
+      },
+      async saveChanges() {
+        try {
+          await axiosInstance.put(`/edituser/${this.selectedUser.id}`, this.selectedUser);
+          const userIndex = this.users.findIndex(user => user.id === this.selectedUser.id);
+          this.users.splice(userIndex, 1, this.selectedUser);
+          this.selectedUser = null;
+        } catch (error) {
+          console.error('Error while trying to save changes', error);
+        }
+      },
+      async deleteUser(userId) {
+        try {
+          await axiosInstance.delete(`/deleteuser/${userId}`);
+          this.users = this.users.filter(user => user.id !== userId);
+          Swal.fire('The user has been deleted!', 'success');
+        } catch (error) {
+          console.error('Error while trying to delete user', error);
+          Swal.fire('Error!', 'There was an issue trying to delete this user', 'error');
+        }
+      },
+      confirmDelete(userId) {
+        Swal.fire({
+          title: 'Are you sure you want delete user?',
+          text: "You won't be able to revert this!",
+          icon: 'warning',
+          showCancelButton: true,
+          confirmButtonColor: '#3085d6',
+          cancelButtonColor: '#d33',
+          confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+          if (result.isConfirmed) {
+            this.deleteUser(userId);
+          }
+        });
+      }
+    }
+  };
+  </script>
+
+  
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
+import 'bootstrap/dist/js/bootstrap.bundle'
+import VueSweetalert2 from 'vue-sweetalert2'
+import 'sweetalert2/dist/sweetalert2.min.css'
 import { createRouter, createWebHistory } from 'vue-router'
 import RegisterUser from './components/RegisterUser.vue'
 import LoginPage from './components/LoginPage.vue'
@@ -19,6 +23,7 @@ const router = createRouter({
 
 const app = createApp(App)
 app.use(router)
+app.use(VueSweetalert2)
 app.mount('#app')
 
 //createApp(App).mount('#app')
